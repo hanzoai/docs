f264e4a9f9e461ce34baf11a6bda9a3813579678
Zach Kelling <z@zeekay.io>
2026-04-30 02:16:51 -0700

    chore: purge PostHog references (Insights brand only)

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       </Body>
       {/* Hanzo Analytics (Umami) */}
       <script defer src="https://analytics.hanzo.ai/script.js" data-website-id="a323a8ae-c811-4061-9626-22caaffc612f" data-do-not-track="true" data-exclude-search="true" />
-      {/* Hanzo Insights (PostHog) */}
+      {/* Hanzo Insights */}
       <script dangerouslySetInnerHTML={{
         __html: `
           !function(t,e){var o,n,p,r;e.__SV||(window.hi=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="hi",u.people=u.people||[],u.toString=function(t){var e="hi";return"hi"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture captureException identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId setPersonProperties".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.hi||[]);
